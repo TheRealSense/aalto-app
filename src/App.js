233f2dbfa8757d58d656e84d841f4fc4071a457a
Mikael Ahlström <mikael.ahlstrom@cgi.com>
2018-11-28 16:24:56 +0200

    [

--- src/App.js
+++ src/App.js
@@ -8,46 +8,42 @@ import Tabs from './auth/Tabs'
 import Nav from './nav/Nav'
 
 class App extends React.Component {
-  state = {
-    user: {},
-    isLoading: true
-  }
-  async componentDidMount() {
-    StatusBar.setHidden(true)
-    try {
-      const user = await Auth.currentAuthenticatedUser()
-      this.setState({ user, isLoading: false })
-    } catch (err) {
-      this.setState({ isLoading: false })
-    }
-  }
-  async componentWillReceiveProps(nextProps) {
-    try {
-      const user = await Auth.currentAuthenticatedUser()
-      this.setState({ user })
-    } catch (err) {
-      this.setState({ user: {} })
-    }
-  }
-  render() {
-    if (this.state.isLoading) return null
-    let loggedIn = false
-    if (this.state.user.username) {
-      loggedIn = true
-    }
-    if (loggedIn) {
-      return (
-        <Nav />
-      )
-    }
-    return (
-      <Tabs />
-    )
-  }
+	state = {
+		user: {},
+		isLoading: true
+	}
+	async componentDidMount() {
+		StatusBar.setHidden(true)
+		try {
+			const user = await Auth.currentAuthenticatedUser()
+			this.setState({ user, isLoading: false })
+		} catch (err) {
+			this.setState({ isLoading: false })
+		}
+	}
+	async UNSAFE_componentWillReceiveProps() {
+		try {
+			const user = await Auth.currentAuthenticatedUser()
+			this.setState({ user })
+		} catch (err) {
+			this.setState({ user: {} })
+		}
+	}
+	render() {
+		if (this.state.isLoading) return null
+		let loggedIn = false
+		if (this.state.user.username) {
+			loggedIn = true
+		}
+		if (loggedIn) {
+			return <Nav />
+		}
+		return <Tabs />
+	}
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+	auth: state.auth
 })
 
 export default connect(mapStateToProps)(App)
